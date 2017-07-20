4646fa662d48fd7e8cdfe37a518228ccd0d352b4
evanleeturner <evanlee.turner@gmail.com>
2017-07-20 12:49:53 -0500

    first upload

---
+++ sonde/formats/read_ysi.py
@@ -0,0 +1,116 @@
+import struct 
+import pandas as pd
+from datetime import datetime
+import pytz
+
+YSI_DEFINITIONS = pd.read_csv("../data/ysi_definitions.csv")
+
+def read_ysi(ysi_file, tzinfo=None):
+        """
+        Read a proprietary YSI sonde instrument file returning as a `pandas` dataframe in UTC timezone
+        
+        Author: evan.turner@twdb.texas.gov 2017/07/20
+        YSI sonde files are 8-bit byte binary files comprised of three sections that appear in order: 
+        1. File metadata
+            * fixed size of file/instrument specific metadata like serial numbers and site in format
+            '<HLH16s32s6sLll36s'
+        2. Sensor and parameter metadata
+            * variable number of rows of fixed column sizes describing the sensors and units.  In format
+            ''<hhHff' This is then translated via the YSI_DEFINITIONS file into human readable header names
+            for the data columns.
+        3. Data rows 
+            * variable number of rows of data.  In format '<l' + str(number of parameters) + 'f' 
+            Column lengths dependent on how many sensor parameter rows were previously encountered 
+            in the previous section of the file.
+        
+        Datetime Considerations
+        The datetime format for YSI is seconds since the 'YSI_epoch' which began march 1, 1984!? Making assumption it was EST?
+        YSI files include no timezone or GPS information.  Therefore, without passing a timezone structure we must assume
+        the instrument recorded data somewhere in the continental USA.
+        
+        Data Considerations
+        Salinity and DO mg/L are calculated in the YSI handset, or the YSI windows program.  The raw binary file either does 
+        not include this (salinity), or reports the sensor mvolts or intermediary constant (in case of DO).  Therefore, this
+        has to be post-processed after reading the binary file.
+        
+        :param ysi_file: filename of YSI binary *.dat file
+        :type ysi_file: String
+        :param tzinfo: Expected timezone of file
+        :type tzinfo: `datetime.tzinfo`
+        :returns:  `pandas.DataFrame`, `Pandas.DataFrame`    
+        """
+        try:
+            fid = open(ysi_file, 'rb')
+        except:
+            print("Error: Could not open file <%s> \n" % ysi_file)
+            raise
+            
+        utc=pytz.utc 
+        if tzinfo:
+            localtime = tzinfo
+        else:
+            localtime = pytz.timezone('US/Eastern')
+            print ("Info: No time zone was set for file, assuming records are recorded in EST")
+            
+        record_type = []
+        num_params = 0
+        data = []
+        parameters = []
+        parameters.append("Datetime_(UTC)")
+        ysi_epoch_in_seconds = 446965200 #time began for YSI on 2004/03/01 !!
+        
+        record_type = fid.read(1)  #read single 8-bit byte
+        while record_type:
+            if record_type == b'A':  #sonde file metadata
+                fmt = '<HLH16s32s6sLll36s'  #little endian: uint,ulong,uint,16char,32char,6char,ulong,long,long,36char
+                fmt_size = struct.calcsize(fmt)
+                instr_type, system_sig, prog_ver, \
+                                 serial_number, site_name, \
+                                 pad1, logging_interval, \
+                                 begin_log_time, \
+                                 first_sample_time, pad2 \
+                                 = struct.unpack(fmt, fid.read(fmt_size))
+                                    
+                #convert time variables into UTC, and cleanup binary format to UTF8                    
+                first_sample_time = datetime.fromtimestamp(first_sample_time + ysi_epoch_in_seconds)\
+                                    .replace(tzinfo=localtime).astimezone(utc)
+                begin_log_time = datetime.fromtimestamp(begin_log_time + ysi_epoch_in_seconds)\
+                                .replace(tzinfo=localtime).astimezone(utc)
+                site_name = site_name.strip(b'\x00').decode("utf-8") 
+                serial_number = serial_number.strip(b'\x00').decode("utf-8") 
+                
+                metadata = pd.DataFrame([(instr_type,system_sig,prog_ver,serial_number,site_name,logging_interval,begin_log_time,
+                                       first_sample_time)], columns=['Instrument_Type', 'System_Signal',
+                                        'Program_Version','Instrument_Serial_Number','Site','Logging_Interval',
+                                        'Begin_Log_Time_(UTC)', 'First_Sample_Time_(UTC)'])
+                
+            elif record_type == b'B':  #row headers of the data
+                num_params = num_params + 1
+                fmt = '<hhHff'  #little endian: short,short,ushort,float,float
+                fmt_size = struct.calcsize(fmt) 
+                channel, sensor,probe,zero_scale,full_scale = struct.unpack(fmt, fid.read(fmt_size))
+                parameters.append(YSI_DEFINITIONS.loc[sensor]["Short_Name"] )
+            elif record_type == b'D':  #actual data rows
+                fmt = '<l' + str(num_params) + 'f'  #little endian, long (our datetime), variable number floats (our data rows)
+                fmt_size = struct.calcsize(fmt)
+                while record_type == b'D':  #repeat importing rows until EOF
+                    row = list(struct.unpack(fmt, fid.read(fmt_size)))
+                    row[0] = datetime.fromtimestamp(row[0] + ysi_epoch_in_seconds)\
+                            .replace(tzinfo=localtime).astimezone(utc)
+                    data.append(row)
+                    record_type = fid.read(1) 
+                #the file has ended, close and return    
+                fid.close()
+                return metadata, pd.DataFrame.from_records(data, columns=parameters)
+            
+            else:        
+                print('Error: Unknown binary formatting found in: <%s>' % ysi_file)
+                break
+                
+            record_type = fid.read(1)
+
+        #something went wrong with file parsing, close and return
+        fid.close()
+        print ("Warning: empty dataframe in file <%s>" % ysi_file)
+        return metadata, pd.DataFrame.from_records(data, columns=parameters)
+    
---
+++ tests/ysi_tests.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+    YSI Format Tests
+    ~~~~~~~~~~~~~~~~
+
+    These tests make sure that the YSI format module is working
+    correctly
+"""
+
+
+import collections
+import csv
+from datetime import datetime
+import os
+
+import nose
+from nose.tools import assert_almost_equal, set_trace
+import numpy as np
+import quantities as pq
+
+from sonde import Sonde
+from sonde import quantities as sq
+from sonde.timezones import cdt, cst
+from sonde.formats import ysi
+
+
+YSI_TEST_FILES_PATH = os.path.join(os.path.dirname(__file__), 'ysi_test_files')
+
+
+def ysi_csv_read(filename):
+    ysi_csv = collections.namedtuple('ysi_csv', 'dates, temps, spconds, depths, odos')
+
+    with open(filename, 'rb') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        # loop through first two header lines
+        for i in range(2):
+            next(csv_file)
+
+        date_list = []
+        temp_list = []
+        spcond_list = []
+        depth_list = []
+        odo_list = []
+
+        for row in csv_reader:
+            timestamp = row[0] + ' ' + row[1]
+            date = datetime.strptime(timestamp, '%m/%d/%y %H:%M:%S')
+            date = date.replace(tzinfo=cdt)
+            date_list.append(date)
+            temp_list.append(row[2])
+            spcond_list.append(row[3])
+            depth_list.append(row[4])
+            odo_list.append(row[5])
+
+        ysi_csv.dates = np.array(date_list)
+        ysi_csv.temps = np.array(temp_list)
+        ysi_csv.spconds = np.array(spcond_list)
+        ysi_csv.depths = np.array(depth_list)
+        ysi_csv.odos = np.array(odo_list)
+
+    return ysi_csv
+
+
+
+def compare_quantity_and_csv_str(quantities, str_list):
+    for quantity, string in zip(quantities, str_list):
+        assert_almost_equal(quantity.base, float(string))
+
+
+
+
+
+#-------------------------------------------------------------------
+# Tests for ysi.YSIReader
+#-------------------------------------------------------------------
+class YSIReaderTestBase():
+    def test_ysi_dates_match_csv(self):
+        for date_pair in zip(self.ysi_reader.dates, self.ysi_csv.dates):
+            assert date_pair[0] == date_pair[1], "%r != %r" % (str(date_pair[0]), str(date_pair[1]))
+
+
+class YSIReader_Test(YSIReaderTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        self.ysi_reader = ysi.YSIReader(ysi_test_file_path, tzinfo=cdt)
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+class YSIReaderExplicitParamDef_Test(YSIReaderTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+        ysi_param_file_path = YSI_TEST_FILES_PATH + '/ysi_param.def'
+
+        self.ysi_reader = ysi.YSIReader(ysi_test_file_path, param_file=ysi_param_file_path, tzinfo=cdt)
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+def YSIReaderNaiveDatetime_Test():
+    """
+    Test that naive datetimes are allowed
+    """
+    ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+    
+    ysi_reader = ysi.YSIReader(ysi_test_file_path)
+    assert ysi_reader.dates != []
+
+#-------------------------------------------------------------------
+
+
+class YSICompareWithCSVTestBase():
+    def test_ysi_dates_match_csv(self):
+        for date_pair in zip(self.ysi_dataset.dates, self.ysi_csv.dates):
+            assert date_pair[0] == date_pair[1], "%r != %r" % (str(date_pair[0]), str(date_pair[1]))
+
+    def test_ysi_temps_match_csv(self):
+        compare_quantity_and_csv_str(self.ysi_dataset.data['water_temperature'], self.ysi_csv.temps)
+
+#    def test_ysi_spconds_match_csv(self):
+#        compare_quantity_and_csv_str(self.ysi_dataset.data['water_specific_conductance'], self.ysi_csv.spconds)
+
+    def test_ysi_depths_match_csv(self):
+        compare_quantity_and_csv_str(self.ysi_dataset.data['water_depth_non_vented'], self.ysi_csv.depths)
+
+    def test_ysi_odos_match_csv(self):
+        compare_quantity_and_csv_str(self.ysi_dataset.data['water_dissolved_oxygen_percent_saturation'], self.ysi_csv.odos)
+
+
+#-------------------------------------------------------------------
+# Tests for ysi.YSIDataset
+#-------------------------------------------------------------------
+class YSIDatasetFilePath_Test(YSICompareWithCSVTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        self.ysi_dataset = ysi.YSIDataset(ysi_test_file_path, tzinfo=cdt)
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+class YSIDatasetFileObject_Test(YSICompareWithCSVTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        with open(ysi_test_file_path, 'rb') as fid:
+            self.ysi_dataset = ysi.YSIDataset(fid, tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+class YSIDatasetFileObject_Test(YSICompareWithCSVTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        with open(ysi_test_file_path, 'rb') as fid:
+            self.ysi_dataset = ysi.YSIDataset(fid, tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+class YSIDatasetExplicitParamFile_Test(YSICompareWithCSVTestBase):
+    """
+    Test that data is read if param_def is explicitly specified
+    """
+
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+        ysi_param_file_path = YSI_TEST_FILES_PATH + '/ysi_param.def'
+
+        self.ysi_dataset = Sonde(ysi_test_file_path, file_format='ysi',
+                                 param_file=ysi_param_file_path, tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+def YSIDatasetNaiveDatetime_Test():
+    ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+    ysi_dataset = ysi.YSIDataset(ysi_test_file_path)
+
+    assert ysi_dataset.dates != []
+
+
+#-------------------------------------------------------------------
+# Tests for the Sonde object with a file_format='ysi'
+#-------------------------------------------------------------------
+class SondeYSIFormatFilePath_Test(YSICompareWithCSVTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        self.ysi_dataset = Sonde(ysi_test_file_path,
+                                 file_format='ysi', tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+
+class SondeYSIFormatFileObject_Test(YSICompareWithCSVTestBase):
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+
+        with open(ysi_test_file_path, 'rb') as fid:
+            self.ysi_dataset = Sonde(fid, file_format='ysi', tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+
+class SondeYSIExplicitParamFile_Test(YSICompareWithCSVTestBase):
+    """
+    Test that data is read if param_def is explicitly specified
+    """
+
+    def setup(self):
+        csv_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.csv'
+        ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+        ysi_param_file_path = YSI_TEST_FILES_PATH + '/ysi_param.def'
+
+        self.ysi_dataset = Sonde(ysi_test_file_path, file_format='ysi',
+                                 param_file=ysi_param_file_path, tzinfo=cdt)
+
+        self.ysi_csv = ysi_csv_read(csv_test_file_path)
+
+
+def SondeYSINaiveDatetime_Test():
+    ysi_test_file_path = YSI_TEST_FILES_PATH + '/BAYT_20070323_CDT_YS1772AA_000.dat'
+    ysi_dataset = Sonde(ysi_test_file_path, file_format='ysi')
+
+    assert ysi_dataset.dates != []
+
+
+if __name__ == '__main__':
+    nose.run()
+
