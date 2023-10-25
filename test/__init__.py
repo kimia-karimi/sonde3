48aea851be8064e481c70093a1719958fd617fd2
Evan L Turner <evanlee.turner@gmail.com>
2023-10-25 13:31:41 -0500

    testing rearrange files

---
+++ test/__init__.py
@@ -0,0 +1,1 @@
+from .test_sonde3 import *
--- test/test_sonde3.py
+++ test/test_sonde3.py
@@ -4,7 +4,7 @@ import unittest
 import sys
 import os
 
-def directory_runner(self,directory,record_length):
+def directory_runner(self,directory, record_length, ftype):
     """
     method traverses a directory of files to open with sonde3.
     """
@@ -12,81 +12,102 @@ def directory_runner(self,directory,record_length):
         with self.subTest(filename=filename):
                 
             f = os.path.join(directory, filename)
+            if ftype == 'pointer':
+                f = open(f, 'rb')
             try:
                 metadata, df = sonde3.sonde(f)
             except:
                 self.fail(msg='failed on file %s' % filename)
             self.assertGreaterEqual(len(df), record_length)
+            if ftype == 'pointer':
+                f.close()
             
             
 
 class TestSonde_CheckBulkFiles(unittest.TestCase):
     """
-    Tests a directory of files to see if the action does not specifically raise an error on each sonde file
+    Tests a directory of files to see if the action does not specifically raise an error on each sonde file.
+    These are the three main manufacturers we need to actively support post 2022.
     """
     def test_ysi(self):
         directory = "test/testfiles/ysi"
-        directory_runner(self,directory,32)
+        directory_runner(self,directory,32,"file")
     def test_aquatroll(self):
         directory = "test/testfiles/aquatroll"
-        directory_runner(self,directory,100)
+        directory_runner(self,directory,100,"file")
     def test_lowell(self):
         directory = "test/testfiles/lowell"
-        directory_runner(self,directory,300)
+        directory_runner(self,directory,300,"file")
+    
+class TestSonde_CheckBulkPointers(unittest.TestCase):
+    """
+    Tests a directory of files to see if the action does not specifically raise an error on each sonde file pointer.
+    These are the three main manufacturers we need to actively support post 2022.
+    """
+    def test_ysi(self):
+        directory = "test/testfiles/ysi"
+        directory_runner(self,directory,32,"pointer")
+    def test_aquatroll(self):
+        directory = "test/testfiles/aquatroll"
+        directory_runner(self,directory,100,"pointer")
+    def test_lowell(self):
+        directory = "test/testfiles/lowell"
+        directory_runner(self,directory,300,"pointer")    
 
 class TestSonde_autodetect_file(unittest.TestCase):
  
     def test_ysi_binary(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/SA08.dat")
+        test_string = sonde3.autodetect("test/testfiles/ysi/SA08.dat")
         self.assertEqual(test_string, 'ysi_binary')
     def test_ysi_cdf(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/SA08.CDF")
+        test_string = sonde3.autodetect("test/testfiles/ysi/SA08.CDF")
         self.assertEqual(test_string, 'ysi_csv')
     def test_ysi_ascii(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/0917GEB.txt")
+        test_string = sonde3.autodetect("test/testfiles/ysi/0917GEB.txt")
         self.assertEqual(test_string, 'ysi_text')       
     def test_ysi_exo(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/GE-SA-B_17H104157_090617_060000.csv")
+        test_string = sonde3.autodetect("test/testfiles/ysi/GE-SA-B_17H104157_090617_060000.csv")
         self.assertEqual(test_string, 'ysi_exo_csv')    
     def test_ysi_exo2(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/EMBData_Retrieved10.3.2019.csv")
+        test_string = sonde3.autodetect("test/testfiles/ysi/EMBData_Retrieved10.3.2019.csv")
         self.assertEqual(test_string, 'ysi_exo2_csv')    
     def test_ysi_exobackup(self):
-        test_string = sonde3.autodetect("tests/ysi_test_files/spotchecks-galveston-180618-081639.csv")
+        test_string = sonde3.autodetect("test/testfiles/ysi/spotchecks-galveston-180618-081639.csv")
         self.assertEqual(test_string, 'ysi_exo_backup')    
     def test_lowell(self):
-        test_string = sonde3.autodetect("tests/lowell_test_files/1603103_SAB_05122016.csv")
+        test_string = sonde3.autodetect("test/testfiles/lowell/1603103_SAB_05122016.csv")
         self.assertEqual(test_string, 'lowell_csv')
     def test_aquatroll(self):
-        test_string = sonde3.autodetect("tests/aquatroll_test_files/HydroVu_AquaTROLL__Baffin_Bay_-1031427_2023-08-15_14-14-09_Export.csv")
+        test_string = sonde3.autodetect("test/testfiles/aquatroll/HydroVu_AquaTROLL__Baffin_Bay_-1031427_2023-08-15_14-14-09_Export.csv")
         self.assertEqual(test_string, 'insitu_csv')
     def test_espey(self):
-        test_string = sonde3.autodetect("tests/espey_test_files/BZ3L_ALL.csv")
+        test_string = sonde3.autodetect("test/testfiles/espey/BZ3L_ALL.csv")
         self.assertEqual(test_string, 'espey_csv')
     def test_eureka(self):
-        test_string = sonde3.autodetect("tests/eureka_test_files/JDM2_20060808_CDT_EU0312.csv")
+        test_string = sonde3.autodetect("test/testfiles/eureka/JDM2_20060808_CDT_EU0312.csv")
         self.assertEqual(test_string, 'eureka_csv')
     def test_generic(self):
-        test_string = sonde3.autodetect("tests/generic_test_files/test1.csv")
+        test_string = sonde3.autodetect("test/testfiles/generic/test1.csv")
         self.assertEqual(test_string,  'generic_csv')
     def test_greenspan_csv(self):
-        test_string = sonde3.autodetect("tests/greenspan_test_files/JDM4_20070410_CST_GS0638.csv")
+        test_string = sonde3.autodetect("test/testfiles/greenspans/JDM4_20070410_CST_GS0638.csv")
         self.assertEqual(test_string,  'greenspan_csv')
     def test_greenspan_txt(self):
-        test_string = sonde3.autodetect("tests/greenspan_test_files/JDM2_20060130_CST_GS0639.txt")
+        test_string = sonde3.autodetect("test/testfiles/greenspan/JDM2_20060130_CST_GS0639.txt")
         self.assertEqual(test_string,   'unsupported_ascii')
     def test_greenspan_xls(self):
-        test_string = sonde3.autodetect("tests/greenspan_test_files/JARD_20080416_CDT_GS9550.xls")
+        test_string = sonde3.autodetect("test/testfiles/greenspan/JARD_20080416_CDT_GS9550.xls")
         self.assertEqual(test_string,   'greenspan_xls')
     def test_hydrolab_txt_a(self):
-        test_string = sonde3.autodetect("tests/hydrolab_test_files/SANT_20070125_CST_HY0000_000.txt")
+        test_string = sonde3.autodetect("test/testfiles/hydrolab/SANT_20070125_CST_HY0000_000.txt")
         self.assertEqual(test_string,  'hydrolab_csv')
     def test_hydrolab_txt_b(self):
-        test_string = sonde3.autodetect("tests/hydrolab_test_files/SANT_20060213_CST_HY4077_000.txt")
+        test_string = sonde3.autodetect("test/testfiles/hydrolab/SANT_20060213_CST_HY4077_000.txt")
         self.assertEqual(test_string,   'hydrolab_csv')
     def test_hydrotech(self):
-        test_string = sonde3.autodetect("tests/hydrotech_test_files/0109CONT.csv")
+        test_string = sonde3.autodetect("test/testfiles/hydrotech/0109CONT.csv")
         self.assertEqual(test_string,   'hydrotech_csv')
+   
     
         
 
@@ -98,25 +119,25 @@ class TestSonde_sonde_filehandle(unittest.TestCase):
     """
    
     def test_sonde_ysi_binary(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/SA08.dat")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/SA08.dat")
         self.assertEqual(len(df), 700)
     def test_sonde_ysi_cdf(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/SA08.CDF")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/SA08.CDF")
         self.assertEqual(len(df), 700)
     def test_sonde_ysi_ascii(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/0917GEB.txt")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/0917GEB.txt")
         self.assertEqual(len(df), 1760)        
     def test_sonde_ysi_exo(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/GE-SA-B_17H104157_090617_060000.csv")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/GE-SA-B_17H104157_090617_060000.csv")
         self.assertEqual(len(df), 1955)    
     def test_sonde_ysi_exo2(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/EMBData_Retrieved10.3.2019.csv")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/EMBData_Retrieved10.3.2019.csv")
         self.assertEqual(len(df), 1775)    
     def test_sonde_ysi_exobackup(self):
-        metadata, df = sonde3.sonde("tests/ysi_test_files/spotchecks-galveston-180618-081639.csv")
+        metadata, df = sonde3.sonde("test/testfiles/ysi/spotchecks-galveston-180618-081639.csv")
         self.assertEqual(len(df), 32)    
     def test_sonde_lowell(self):
-        metadata, df = sonde3.sonde("tests/lowell_test_files/1603103_SAB_05122016.csv")
+        metadata, df = sonde3.sonde("test/testfiles/lowell/1603103_SAB_05122016.csv")
         self.assertEqual(len(df), 3026)  
                
 
@@ -126,37 +147,37 @@ class TestSonde_sonde_filepointer(unittest.TestCase):
     This speficic class tests a filepointer to be passed to sonde3, as it would be for the WDFT package.
     """
     def test_sonde_ysi_binary(self):
-        fid = open("tests/ysi_test_files/SA08.dat", 'rb')
+        fid = open("test/testfiles/ysi/SA08.dat", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 700)
         fid.close
     def test_sonde_ysi_cdf(self):
-        fid = open("tests/ysi_test_files/SA08.CDF", 'rb')
+        fid = open("test/testfiles/ysi/SA08.CDF", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 700)
         fid.close
     def test_sonde_ysi_ascii(self):
-        fid = open("tests/ysi_test_files/0917GEB.txt", 'rb')
+        fid = open("test/testfiles/ysi/0917GEB.txt", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 1760) 
         fid.close
     def test_sonde_ysi_exo(self):
-        fid = open("tests/ysi_test_files/GE-SA-B_17H104157_090617_060000.csv", 'rb')
+        fid = open("test/testfiles/ysi/GE-SA-B_17H104157_090617_060000.csv", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 1955)  
         fid.close
     def test_sonde_ysi_exo2(self):
-        fid = open("tests/ysi_test_files/EMBData_Retrieved10.3.2019.csv", 'rb')
-        metadata, df = sonde3.sonde("tests/ysi_test_files/EMBData_Retrieved10.3.2019.csv")
+        fid = open("test/testfiles/ysi/EMBData_Retrieved10.3.2019.csv", 'rb')
+        metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 1775)
         fid.close()
     def test_sonde_ysi_exobackup(self):
-        fid = open("tests/ysi_test_files/spotchecks-galveston-180618-081639.csv", 'rb')
+        fid = open("test/testfiles/ysi/spotchecks-galveston-180618-081639.csv", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 32)    
         fid.close()
     def test_sonde_lowell(self):
-        fid = open("tests/lowell_test_files/1603103_SAB_05122016.csv", 'rb')
+        fid = open("test/testfiles/lowell/1603103_SAB_05122016.csv", 'rb')
         metadata, df = sonde3.sonde(fid)
         self.assertEqual(len(df), 3026)  
         fid.close()
